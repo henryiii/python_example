114edfe4033dc940f5f13d17af391cdb14226cc7
Henry Schreiner <henryschreineriii@gmail.com>
2020-09-17 21:34:31 -0400

    feat: support setup helpers

--- conda.recipe/bld.bat
+++ conda.recipe/bld.bat
@@ -2,5 +2,5 @@ if "%ARCH%" == "32" (set PLATFORM=x86) else (set PLATFORM=x64)
 call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" %PLATFORM%
 set DISTUTILS_USE_SDK=1
 set MSSdk=1
-"%PYTHON%" setup.py install
+"%PYTHON%" -m pip install .
 if errorlevel 1 exit 1
